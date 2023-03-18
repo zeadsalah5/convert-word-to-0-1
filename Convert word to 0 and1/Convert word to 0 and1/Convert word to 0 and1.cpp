f1a9d87519d7e4118a8cd35f3912d8c62330df2c
zeadsalah5 <115894389+zeadsalah5@users.noreply.github.com>
2023-03-18 12:47:56 -0700

    Add files via upload

---
+++ Convert word to 0 and1/Convert word to 0 and1/Convert word to 0 and1.cpp
@@ -0,0 +1,319 @@
+// Convert word to 0 and1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	char str[100];
+	
+
+
+	cout << "Enter some text :-" << endl ;
+	cout << "______________________________________" << endl ;
+	cin>>str;
+	cout << str;
+	cout << "\n______________________________________" << endl ;
+
+
+	for (int b = 0; str[b] != '\0'; b++) {
+
+		switch (str[b])
+		{
+		case 'a':
+		{
+			cout << " 01100001 ";
+			break;
+		}
+		
+		case 'b':
+		{
+			cout << " 01100010 ";
+			break;
+		}
+
+		case 'c':
+		{
+			cout << " 01100011 ";
+			break;
+		}
+		case 'd':
+		{
+			cout << " 01100100 ";
+			break;
+		}
+		case 'e':
+		{
+			cout << " 01100101 ";
+			break;
+		}
+		case 'f':
+		{
+			cout << " 01100110 ";
+			break;
+		}
+		case 'g':
+		{
+			cout << " 01100111 ";
+			break;
+		}
+		case 'h':
+		{
+			cout << " 01101000 ";
+			break;
+		}
+		case 'i':
+		{
+			cout << " 01101001 ";
+			break;
+		}
+		case 'j':
+		{
+			cout << " 01101010 ";
+			break;
+		}
+		case 'k':
+		{
+			cout << " 01101011 ";
+			break;
+		}
+		case 'l':
+		{
+			cout << " 01101100 ";
+			break;
+		}
+		case 'm':
+		{
+			cout << " 01101110 ";
+			break;
+		}
+		case 'n':
+		{
+			cout << " 01101110 ";
+			break;
+		}
+		case 'o':
+		{
+			cout << " 01101111 ";
+			break;
+		case 'p':
+		{
+			cout << " 01110000 ";
+			break;
+		}
+		case 'q':
+		{
+			cout << " 01110000 ";
+			break;
+		case 'r':
+		{
+			cout << " 01110010 ";
+			break;
+		}
+		case 's':
+		{
+			cout << " 01110011 ";
+			break;
+		}
+		case 't':
+		{
+			cout << " 0111010 ";
+			break;
+		}
+		case 'u':
+		{
+			cout << " 01110101 ";
+			break;
+		}
+		case 'v':
+		{
+			cout << " 01110110 ";
+			break;
+		}
+		case 'w':
+		{
+			cout << " 01110111 ";
+			break;
+		}
+		case 'x':
+		{
+			cout << " 01111000 ";
+			break;
+
+		}
+		case 'y':
+		{
+			cout << " 01111001 ";
+			break;
+		}
+		case 'z':
+		{
+			cout << " 01111010 ";
+			break;
+		}
+		case 'A':
+		{
+			cout << " 01000001 ";
+			break;
+		}
+
+		case 'B':
+		{
+			cout << " 01000010 ";
+			break;
+		}
+
+		case 'C':
+		{
+			cout << " 01000011 ";
+			break;
+		}
+		case 'D':
+		{
+			cout << " 01000100 ";
+			break;
+		}
+		case 'E':
+		{
+			cout << " 01000110 ";
+			break;
+		}
+		case 'F':
+		{
+			cout << " 01000110 ";
+			break;
+		}
+		case 'G':
+		{
+			cout << " 01000111 ";
+			break;
+		}
+		case 'H':
+		{
+			cout << " 01001000 ";
+			break;
+		}
+		case 'I':
+		{
+			cout << " 01001001 ";
+			break;
+		}
+		case 'J':
+		{
+			cout << " 01001010 ";
+			break;
+		}
+		case 'K':
+		{
+			cout << " 01001011 ";
+			break;
+		}
+		case 'L':
+		{
+			cout << " 01001100 ";
+			break;
+		}
+		case 'M':
+		{
+			cout << " 01001101 ";
+			break;
+		}
+		case 'N':
+		{
+			cout << " 01001110 ";
+			break;
+		}
+		case 'O':
+		{
+			cout << " 01001111 ";
+			break;
+		case 'P':
+		{
+			cout << " 01010000 ";
+			break;
+		}
+		case 'Q':
+		{
+			cout << " 01010001 ";
+			break;
+		case 'R':
+		{
+			cout << " 01010010 ";
+			break;
+		}
+		case 'S':
+		{
+			cout << " 01010011 ";
+			break;
+		}
+		case 'T':
+		{
+			cout << " 01010100 ";
+			break;
+		}
+		case 'U':
+		{
+			cout << " 01010101 ";
+			break;
+		}
+		case 'V':
+		{
+			cout << " 01010110 ";
+			break;
+		}
+		case 'W':
+		{
+			cout << " 01010111 ";
+			break;
+		}
+		case 'X':
+		{
+			cout << " 01011000 ";
+			break;
+		}
+		case 'Y':
+		{
+			cout << " 01011001 ";
+			break;
+		}
+		case 'Z':
+		{
+			cout << " 01011010 ";
+			break;
+		}
+		case '0':
+		{
+			cout << "";
+			break;
+		}
+		default:
+		{
+			cout << "\n ";
+			cout << "\n ";
+			cout << "\n ";
+			cout << "\n ";
+			cout << "\n ";
+			cout << "\n \n ";
+			break;
+		}
+		}
+
+		return 0;
+
+		}
+		}
+		}
+		}
+
+	}
+	/*cout << "\t______________________________________" << endl;
+	cout << "\n\t       And if you liked it,        " << endl;
+	cout << " \tdon't forget to give a (+1) like!   " << endl;
+	cout << "  \t          Thank you!               " << endl;
+	cout << "\t______________________________________" << endl << endl;*/
+}
+
+
